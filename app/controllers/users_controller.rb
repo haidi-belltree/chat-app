b949738e13c5fed4616b68c330f0f4b8d5d7f1b8
haidi-belltree <3p1erfv.mbah8yj4higu@gmail.com>
2020-12-31 19:10:54 +0900

    ユーザー編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,11 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(user_params)
-      redirect_to user_path
+      redirect_to root_path
     else
       render :edit
     end
+
   end
 
   private
